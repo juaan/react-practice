4028cc803d9dac429d9468d50e23d3d308b0f024
anthony juan <anthonyjuanchristian@gmail.com>
2017-11-22 23:59:14 +0700

    adding some simple test for ShowCard component

--- js/__test__/Search.test.jsx
+++ js/__test__/Search.test.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
+import preload from '../../data.json'
+import ShowCard from '../ShowCard';
 import Search from '../Search';
 
 test('Search render correctly', () => {
     const component = shallow(<Search />)
     expect(component).toMatchSnapshot();
 })
+
+
+test('Search should render correct amount of shows', () => {
+    const component = shallow(<Search />)
+    expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
+
+xtest('search should render correct amount of shows based on search term', () => {
+
+});
